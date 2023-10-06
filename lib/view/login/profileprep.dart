535927d0a1cecd5c99226346234650c36127ee8f
adelacalista8 <adela.825210067@stu.untar.ac.id>
2023-10-06 15:15:11 +0700

    navbar1

---
+++ lib/view/login/profileprep.dart
@@ -0,0 +1,200 @@
+import 'package:UTS_Kelompok5/common/colo_extension.dart';
+import 'package:UTS_Kelompok5/view/login/login_view.dart';
+import 'package:flutter/material.dart';
+
+import '../../common_widget/round_orangebutton.dart';
+import '../../common_widget/round_textfield.dart';
+
+class CompleteProfileView extends StatefulWidget {
+  const CompleteProfileView({super.key});
+
+  @override
+  State<CompleteProfileView> createState() => _CompleteProfileViewState();
+}
+
+class _CompleteProfileViewState extends State<CompleteProfileView> {
+  TextEditingController txtDate = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: TColor.black,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/img/twogirls.png",
+                  width: 250,
+                  height: 250,
+                ),
+                SizedBox(
+                  height: media.width * 0.01,
+                ),
+                Text(
+                  "Let’s complete your profile",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  "It'll certainly help us know more about you!",
+                  style: TextStyle(color: Colors.grey[300], fontSize: 12),
+                ),
+                SizedBox(
+                  height: media.width * 0.05,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: TColor.lightGray,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Row(
+                          children: [
+                            Container(
+                                alignment: Alignment.center,
+                                width: 50,
+                                height: 50,
+                                padding: const EdgeInsets.symmetric(horizontal: 15),
+
+                                child: Image.asset(
+                                  "assets/img/gender.png",
+                                  width: 20,
+                                  height: 20,
+                                  fit: BoxFit.contain,
+                                  color: TColor.gray,
+                                )),
+
+                            Expanded(
+                              child: DropdownButtonHideUnderline(
+                                child: DropdownButton(
+                                  items: ["Male", "Female"]
+                                      .map((name) => DropdownMenuItem(
+                                    value: name,
+                                    child: Text(
+                                      name,
+                                      style: TextStyle(
+                                          color: TColor.gray,
+                                          fontSize: 14),
+                                    ),
+                                  ))
+                                      .toList(),
+                                  onChanged: (value) {},
+                                  isExpanded: true,
+                                  hint: Text(
+                                    "Choose Gender",
+                                    style: TextStyle(
+                                        color: TColor.gray, fontSize: 12),
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            const SizedBox(width: 8,)
+
+                          ],
+                        ),),
+                      SizedBox(
+                        height: media.width * 0.04,
+                      ),
+                      RoundTextField(
+                        controller: txtDate,
+                        hitText: "Date of Birth",
+                        icon: "assets/img/date.png",
+                      ),
+                      SizedBox(
+                        height: media.width * 0.04,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: RoundTextField(
+                              controller: txtDate,
+                              hitText: "Your Weight",
+                              icon: "assets/img/weight.png",
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Container(
+                            width: 50,
+                            height: 50,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: TColor.tealmixbrown,
+                              ),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Text(
+                              "KG",
+                              style:
+                              TextStyle(color: TColor.black, fontSize: 12, fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: media.width * 0.04,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: RoundTextField(
+                              controller: txtDate,
+                              hitText: "Your Height",
+                              icon: "assets/img/hight.png",
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Container(
+                            width: 50,
+                            height: 50,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: TColor.tealmixbrown,
+                              ),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Text(
+                              "CM",
+                              style:
+                              TextStyle(color: TColor.black, fontSize: 12, fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: media.width * 0.07,
+                      ),
+                      RoundButton(
+                          title: "Proceed to Login",
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                    const LoginView()));
+                          }),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
